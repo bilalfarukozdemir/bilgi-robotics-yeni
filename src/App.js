70503fb4bf64878edede9974d9400faf11dc24e1
bilalfarukozdemir <bilal.ozdemir@bilgiedu.net>
2024-08-20 15:35:19 +0300

    güncelleme

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Sponsor from './components/Sponsor';
 import Profile from './components/Profile';
 import Auth from './components/Auth';
 import SocialFeed from './components/Socialfeed';
+import Login from './components/Login';
+import UserProfile from './components/Userprofile';
+import Register from './components/Register';
+
+
 
 function App() {
   return (
@@ -17,6 +22,10 @@ function App() {
         <Route path="/auth" element={<Auth />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/feed" element={<SocialFeed />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/user/:userId" element={<UserProfile />} />
+
       </Routes>
     </Router>
   );
